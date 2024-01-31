eb254dabe0040d41c671aa777baaa5ee4562fbd3
WARAGON <63374544+wp1631@users.noreply.github.com>
2024-01-31 15:17:29 +0700

    Add submit reactive

--- colorwheel/src/components/CWheel.vue
+++ colorwheel/src/components/CWheel.vue
@@ -1,10 +1,10 @@
 <template>
     <div id="container">
-        <color-picker v-bind="color" variant="persistent" @input="onInput" @select="onColorSelect"/>
-        <h1 v-text="color.hue.toFixed(3)"></h1>
+        <color-picker v-bind="{color, msg}" variant="persistent" @input="onInput" @select="onColorSelect"/>
+        <h1 v-text="color.hue.toFixed(3)"></h1> <h2 v-text="msg.msg"></h2>
         <div id="response">
-        <button >Submit</button>
-        <button @click="()=>color.hue=(Math.random()*360)">Random</button>
+        <button @click.passive="()=>msg.msg='sent'">Submit</button>
+        <button @click.passive="()=>{color.hue=(Math.random()*360); msg.msg=''}">Random</button>
         </div>
     </div>
 </template>
@@ -12,10 +12,14 @@
 <script lang="ts">
     import { reactive } from 'vue';
     import ColorPicker from '@radial-color-picker/vue-color-picker';
+    let msg: string;
 
     export default {
         components: { ColorPicker },
         setup() {
+            const msg = reactive({
+                msg: "",
+            });
             const color = reactive({
                 hue: 50,
                 saturation: 100,
@@ -25,6 +29,7 @@
 
             return {
                 color,
+                msg,
                 onInput(hue: number) {
                     console.log("Hue changed to: " + hue);
                     color.hue = hue;
